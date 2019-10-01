ceb8a21c261f94ef956d42927b6a7b2c40f404d2
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-09-30 22:44:42 -0400

    Register validation add

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ function login() {
 
     var loginBtn = document.getElementById('btnLogin');
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
     var data = {
-        username: username,
-        password: password,
-        email: username
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+        email: document.getElementById('username').value
     };
 
     loginBtn.setAttribute('disabled', true);
@@ -27,7 +24,7 @@ function login() {
             } else if (response.status === 201) {
                 loginBtn.textContent = 'Loged!';
             }
-            
+
             return response.json();
         })
         .then(response => {
@@ -40,10 +37,47 @@ function login() {
 
 }
 
-function validateForm(form) {
+function register() {
+    var registerBtn = document.getElementById('btnRegister');
 
+    var data = {
+        name: document.getElementById('fullName').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password1').value
+    };
+
+    registerBtn.setAttribute('disabled', true);
+
+    fetch(`${API_PATH}/register`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+
+            if (response.status === 400) {
+                registerBtn.removeAttribute('disabled');
+                document.getElementById('emailExtisOnRegister').classList.remove('hide');
+                document.getElementById('emailExtisOnRegister').classList.add('show');
+            } else if (response.status === 201) {
+                registerBtn.textContent = 'Registered!';
+            }
+
+            return response.json();
+        })
+        .then(response => {
+            console.log('Success:', response)
+        })
+        .catch(error => {
+            registerBtn.removeAttribute('disabled');
+            console.log(error);
+        })
+}
+
+function validateForm(form) {
+    event.preventDefault();
     if (form.checkValidity() === false) {
-        event.preventDefault();
         event.stopPropagation();
     } else {
         if (form.getAttribute('id') === 'registerForm') {
