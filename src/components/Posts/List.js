a13fd312e9f8e8325f3b69a8e0497a3d4aec1e22
itsalb3rt <itsalb3rt@gmail.com>
2019-10-10 10:00:05 -0400

    UX

--- src/components/Posts/List.js
+++ src/components/Posts/List.js
@@ -10,31 +10,37 @@ var template = `
     </h5>
     <h6 class="card-subtitle mb-2 text-muted">by: {{NAME}} - {{EMAIL}}, <span style='color: grey'> {{DATE}}</span></h6>
     <p class="card-text">{{BODY}}</p>
-    <a href="#post/1" class="card-link">Read more...</a>
+    <div>Views: {{VIEWS}} Comments: {{COMMENTS}} Likes: {{LIKES}}</div>
+    <a href="#post/1" class="card-link float-right">Read more...</a>
   </div>
 </div>
 `
 
 class List extends Route {
 
-    constructor(){
-        super('posts', { htmlName : 'public/views/Posts/List.html' });
+    constructor() {
+        super('posts', {
+            htmlName: 'public/views/Posts/List.html'
+        });
         this.onMountCb = this.whenMounted
     }
 
-    async whenMounted(){
+    async whenMounted() {
         document.getElementById('posts').innerHTML = '<h3>Loading Posts</h3>'
         let temporalTemplate = '';
 
-        const posts = await store.actions().getPosts();        
-        
+        const posts = await store.actions().getPosts();
+
         posts.forEach(post => {
-            temporalTemplate+= template
-            .replace('{{TITLE}}', post.title)
-            .replace('{{BODY}}', post.body.substring(0,100))
-            .replace('{{NAME}}', post.userName)
-            .replace('{{EMAIL}}', post.userEmail)
-            .replace('{{DATE}}', moment(post.createdAt).format('DD/MM/YYYY h:mm:ss a'))
+            temporalTemplate += template
+                .replace('{{TITLE}}', post.title)
+                .replace('{{BODY}}', post.body.substring(0, 100))
+                .replace('{{NAME}}', post.userName)
+                .replace('{{EMAIL}}', post.userEmail)
+                .replace('{{VIEWS}}', post.views)
+                .replace('{{COMMENTS}}', post.comments)
+                .replace('{{LIKES}}', post.likes)
+                .replace('{{DATE}}', moment(post.createdAt).format('DD/MM/YYYY h:mm:ss a'))
         });
 
         document.getElementById('posts').innerHTML = temporalTemplate
