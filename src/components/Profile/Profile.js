9d8d6552394b64ba510185742a60a99f12cd0d99
itsalb3rt <itsalb3rt@gmail.com>
2019-10-31 20:08:35 -0400

    add generic profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,58 @@
+import Route from '../../libs/route';
+import moment from 'moment';
+import Miscellany from '../Miscellany/Loading';
+
+var template = `
+<div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="./images/profile-placeholder.jpg" class="card-img" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{USERNAME}}</h5>
+          <p class="card-text">
+              <div>{{EMAIL}}</div>
+              <div>{{POSTS}} - <a href="#">View All</a></div>
+              <div>{{REGISTERAT}}</div>
+          </p>
+        </div>
+      </div>
+    </div>
+`
+
+class Profile extends Route {
+
+    constructor() {
+        super('profile', {
+            htmlName: './views/Profile/Profile.html'
+        });
+        this.onMountCb = this.whenMounted;
+    }
+
+    async whenMounted() {
+
+        document.getElementById('profile').innerHTML = Miscellany.loading;
+
+        let temporalTemplate = '';
+        const userId = window.location.hash.substring(window.location.hash.indexOf('/') + 1);
+        let user;
+
+        if (userId == 'me') {
+            user = await window.me;
+        } else {
+            user = await store.actions().getUser(userId);
+        }
+
+        temporalTemplate += template
+            .replace('{{POSTS}}', user.posts)
+            .replace('{{USERNAME}}', user.name)
+            .replace('{{EMAIL}}', user.email)
+            .replace('{{REGISTERAT}}', moment(user.createdAt).format('DD/MM/YYYY h:mm:ss'));
+
+        document.getElementById('profile').innerHTML = temporalTemplate;
+
+    }
+}
+
+export default new Profile();
