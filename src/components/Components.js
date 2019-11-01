10e0c192afc172b1eb7a2fd088d8c2756d7e9d2b
itsalb3rt <itsalb3rt@gmail.com>
2019-10-31 22:24:12 -0400

    add component

--- src/components/Components.js
+++ src/components/Components.js
@@ -3,5 +3,6 @@ import Logout from './Auth/Logout';
 import List from './Posts/List';
 import Post from './Posts/post';
 import Profile from './Profile/Profile';
+import CreatePost from './Posts/create';
 
-export {Auth,Logout,List,Post, Profile};
+export {Auth,Logout,List,Post,CreatePost, Profile};
